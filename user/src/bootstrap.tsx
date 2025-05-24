771c519023cf693233c0447f3f9a8ffa6eb7a586
Mayukh Basu <roxy.firebird@gmail.com>
2025-05-25 00:40:04 +0530

    individually made sure apps are running at 4201 port

--- user/src/bootstrap.tsx
+++ user/src/bootstrap.tsx
@@ -1,14 +1,14 @@
-import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
 
 import App from './app/app';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <StrictMode>
+  <GoogleOAuthProvider clientId="294043054811-dv6ta4ftcth7tq7jjgv4mqcapbge367t.apps.googleusercontent.com">
     <App />
-  </StrictMode>
+  </GoogleOAuthProvider>
 );
